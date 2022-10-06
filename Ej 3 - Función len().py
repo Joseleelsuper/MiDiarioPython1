11946340c13341e911ce1781d4e8fb165b8c2df1
José Gallardo <51025490+Joseleelsuper@users.noreply.github.com>
2022-10-06 18:20:22 +0200

    Add files via upload

---
+++ Ej 3 - Función len().py
@@ -0,0 +1,16 @@
+# Definir una función que calcule la longitud de una lista o una cadena dada. 
+# (Es cierto que python tiene la función len() incorporada, pero escribirla por nosotros mismos resulta un muy buen ejercicio).
+
+def long(l):
+    x = 0
+    for i in l:
+        x+=1
+    print("La longitud de la cadena de carácteres es: ",x)
+
+    exit()
+
+def main():
+    l = str(input("Escriba una cadena de carácteres: "))
+    long(l)
+
+main()
