abd4e35a3cd2d7c3db2127a0828757d97e552ed0
José Gallardo <51025490+Joseleelsuper@users.noreply.github.com>
2022-10-06 18:48:55 +0200

    Add files via upload

---
+++ Ej 5 - Funciones sum() y multip().py
@@ -0,0 +1,29 @@
+#Escribir una función sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista. 
+#Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+
+def sum(lista):
+    x = 0
+    for i in lista:
+        x+=i
+    
+    print("La suma de los números de la lista es: ",x)
+
+def multip(lista):
+    x = 1
+    for i in lista:
+        x*=i
+
+    print("La multiplicación de los números de la lista es: ",x)
+
+def main():
+    lista = [0]*100
+    x = int(input("Cuantos números quieres en la lista: "))
+    for i in range(x):
+        lista[i] = int(input(f"Escribe el número {i+1}: "))
+    lista = lista[:x]
+    sum(lista)
+    multip(lista)
+
+    exit()
+
+main()
