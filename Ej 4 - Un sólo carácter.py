ff35cfd4d6c5746ce268c19514f92520b9446772
José Gallardo <51025490+Joseleelsuper@users.noreply.github.com>
2022-10-07 15:59:20 +0200

    Update Ej 4 - Un sólo carácter.py

--- Ej 4 - Un sólo carácter.py
+++ Ej 4 - Un sólo carácter.py
@@ -1,7 +1,8 @@
 #Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.
 
 def vocal(v):
-    if (v == 'a' or v == 'e' or v == 'i' or v == 'o' or v =='u'):
+    if (v == 'a' or v == 'e' or v == 'i' or v == 'o' or v == 'u' or
+       v == 'A' or v == 'E' or v == 'I' or v == 'O' or v == 'U'):
         print("True")
     else:
         print("False")
